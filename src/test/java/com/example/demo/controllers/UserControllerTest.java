2488fa33215e037199cb1234a977f643025fe425
Christian Sasse (Nilsson) <chrillen@gmail.com>
2020-11-15 19:52:10 +0100

    checking in code locally

---
+++ src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,49 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+public class UserControllerTest {
+    private UserController userController;
+    private UserRepository userRepository = mock(UserRepository.class);
+    private CartRepository cartRepository = mock(CartRepository.class);
+    private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+
+    @Before
+    public void setUp() {
+        this.userController = new UserController();
+        TestUtils.injectObject(userController,"userRepository",userRepository);
+        TestUtils.injectObject(userController,"cartRepository",cartRepository);
+        TestUtils.injectObject(userController,"bCryptPasswordEncoder",encoder);
+
+    }
+
+    @Test
+    public void create_user_happy_path() throws Exception {
+        CreateUserRequest r = new CreateUserRequest();
+        r.setUsername("test");
+        r.setPassword("testpassword");
+        r.setConfirmPassword("testpassword");
+        final ResponseEntity<User> response = userController.createUser(r);
+        assertNotNull(response);
+        assertEquals(200,response.getStatusCodeValue());
+
+        User u = response.getBody();
+        assertNotNull(u);
+        assertEquals(0,u.getId());
+        assertEquals("test",u.getUsername());
+        //assertEquals("testpassword",u.getPassword());
+    }
+}
